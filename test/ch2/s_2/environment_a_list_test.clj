a6cb5c50820a379e04cdee26e23e1771eca8bd57
José Albert Cruz Almaguer <jalbertcruz@gmail.com>
2016-05-01 16:52:37 -0400

    association-list implementation for environment added.

---
+++ test/ch2/s_2/environment_a_list_test.clj
@@ -0,0 +1,25 @@
+;;
+;; Author José Albert Cruz Almaguer <jalbertcruz@gmail.com>
+;; Copyright 2016 by José Albert Cruz Almaguer.
+;;
+;; This program is licensed to you under the terms of version 3 of the
+;; GNU Affero General Public License. This program is distributed WITHOUT
+;; ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+;; MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+;; AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+;;
+
+(ns ch2.s-2.environment-a-list_test
+  (:require [clojure.test :refer :all]
+            [ch2.s-2.environment-a-list :refer :all]))
+
+(deftest exercise-2-5
+  (testing "environment a-list (data structure) representation --> Sec 2.2.2, exercise 2.5"
+
+    (let [e (extend-env 'd 6
+                        (extend-env 'y 8
+                                    (extend-env 'x 7
+                                                (extend-env 'y 14
+                                                            (empty-env)))))]
+
+      (is (= 8 (apply-env e 'y))))))
