a63b8e3c670a9c9c3ff02f2d0c159c39cc76201b
José Albert Cruz Almaguer <jalbertcruz@gmail.com>
2016-05-01 13:34:18 -0400

    Stack procedural implementation added.

---
+++ src/ch2/s_2/stack_proc.clj
@@ -0,0 +1,35 @@
+;;
+;; Author José Albert Cruz Almaguer <jalbertcruz@gmail.com>
+;; Copyright 2016 by José Albert Cruz Almaguer.
+;;
+;; This program is licensed to you under the terms of version 3 of the
+;; GNU Affero General Public License. This program is distributed WITHOUT
+;; ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+;; MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+;; AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+;;
+
+(ns ch2.s-2.stack-proc)
+
+(def empty-stack (fn []
+                   (fn [dispatch]
+                     (case dispatch
+                       '(:top :pop) (println "Error")
+                       :empty-stack? true))))
+
+(def push (fn [e s]
+            (fn [dispatch]
+              (case dispatch
+                :top e
+                :pop s
+                :empty-stack? false))))
+
+(def top (fn [s]
+           (s :top)))
+
+(def pop (fn [s]
+           (s :pop)))
+
+(def empty-stack? (fn [s]
+                    (s :empty-stack?)))
+
---
+++ test/ch2/s_2/stack_proc_test.clj
@@ -0,0 +1,31 @@
+;;
+;; Author José Albert Cruz Almaguer <jalbertcruz@gmail.com>
+;; Copyright 2016 by José Albert Cruz Almaguer.
+;;
+;; This program is licensed to you under the terms of version 3 of the
+;; GNU Affero General Public License. This program is distributed WITHOUT
+;; ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+;; MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+;; AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+;;
+
+(ns ch2.s-2.stack-proc_test
+  (:require [clojure.test :refer :all]
+            [ch2.s-2.stack-proc :refer :all]))
+
+(deftest exercise-2-12-sol-dispatch
+  (testing "stack --> 2.12"
+    (let [e (pop
+             (pop
+              (push 2
+                    (push 6
+                          (push 3
+                                (empty-stack))))))]
+
+      (is (= 3 (top e)))
+
+      (is (= (empty-stack? e) false))
+
+      (is (empty-stack? (pop e)))
+
+      (is (empty-stack? (empty-stack))))))
